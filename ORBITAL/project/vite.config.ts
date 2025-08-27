b991120d0084bec306e3761cc52d1c9f513bcad6
mr6urns <blake@intrana.io>
2025-08-26 20:50:12 -0500

    Fix Vite configuration root path

--- ORBITAL/project/vite.config.ts
+++ ORBITAL/project/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
+  root: 'ORBITAL/project',
   plugins: [
     react(),
     legacy({
