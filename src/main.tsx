72647f2637b3f6e54a92d0ba98a7642176a915b0
mr6urns <blake@intrana.io>
2025-08-26 20:49:30 -0500

    Remove diff markers from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,23 @@
-@@ .. @@
- const rootElement = document.getElementById('root');
- if (!rootElement) {
-   const root = document.createElement('div');
-   root.id = 'root';
-   document.body.appendChild(root);
-+  
-+  createRoot(root).render(
-+    <StrictMode>
-+      <App />
-+    </StrictMode>
-+  );
-+} else {
-+  createRoot(rootElement).render(
-+    <StrictMode>
-+      <App />
-+    </StrictMode>
-+  );
- }
--
--createRoot(rootElement || document.getElementById('root')!).render(
--  <StrictMode>
--    <App />
--  </StrictMode>
--);
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  const root = document.createElement('div');
+  root.id = 'root';
+  document.body.appendChild(root);
+  
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
