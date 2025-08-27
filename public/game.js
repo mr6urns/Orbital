f0b82886dd590768afe76b4c2acf16d30d99077d
mr6urns <blake@intrana.io>
2025-08-26 21:13:45 -0500

    Fix player rotation and restore walking animations

--- public/game.js
+++ public/game.js
@@ -893,15 +893,10 @@ function updatePlayer(delta) {
 
     player.position.copy(nextPosition);
 
-    // Make player body face the camera direction
-    const cameraDirection = new THREE.Vector3(0, 0, -1).applyQuaternion(currentCameraRotation);
-    const playerBodyRotation = new THREE.Matrix4();
-    playerBodyRotation.lookAt(
-        player.position,
-        player.position.clone().add(cameraDirection),
-        playerUp
-    );
-    player.quaternion.setFromRotationMatrix(playerBodyRotation);
+    // Keep player upright without rotating with camera
+    player.rotation.x = 0;
+    player.rotation.y = 0;
+    player.rotation.z = 0;
 }
 
 function updateCamera() {
